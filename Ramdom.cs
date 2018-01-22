c0fb909f2ba2dcd89d69e5580ea737bdb3fe2563
Royal <royal@Royals-MacBook-Pro.local>
2018-01-22 23:48:42 +0700

    push class

---
+++ Ramdom.cs
@@ -0,0 +1,37 @@
+class Random {
+    public static int[] CreateRandomArr(int start = 0, int length = 1)
+    {
+        if (start < 1)
+            start = 0;
+
+        if (length < 1)
+            length = 1;
+
+        if (length <= 1)
+            return new int[1] { 0 };
+
+        int[] array = Enumerable.Range(start, length).ToArray();
+
+        int first = 0;
+        while (true)
+        {
+            if (first > length - 1)
+                break;
+
+            var rand = new Random(DateTime.Now.Millisecond);
+            var next = rand.Next(first, length - 1);
+            var next2 = rand.Next(0, first > length / 2 ? first : length / 2);
+
+            int tmp = array[first];
+            array[first] = array[next];
+            array[next] = tmp;
+
+            tmp = array[next];
+            array[next] = array[next2];
+            array[next2] = tmp;
+
+            first++;
+        }
+        return array;
+    }
+}
